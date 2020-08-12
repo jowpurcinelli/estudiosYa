7e341e8357d6bbe637ea78e76e7e093f84ce2443
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-08-12 01:34:53 -0300

    teacherListandStyle

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React from "react";
+import "./styles.css";
 
-export default function TeacherList( ) {
-    return (
-    <div>
-        <h1>Thats my teacher</h1>
+import Header from "../../components/Header/";
+import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
+
+export default function TeacherList() {
+  return (
+    <div className="container" id="page-teacher-list">
+      <Header title="Eses son los Profesores disponibles.">
+        <form id="search-teachers">
+          <Select
+            name="subject"
+            label="Clases"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "Matematica", label: "Matematica" },
+              { value: "Ingles", label: "Ingles" },
+              { value: "Historia", label: "Historia" },
+              { value: "Fisica", label: "Fisica" },
+              { value: "Portugues", label: "Portugues" },
+              { value: "Quimica", label: "Quimica" },
+              {value: "Filosofia", label: "Filosofia"},
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia de la Semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Lunes" },
+              { value: "2", label: "Martes" },
+              { value: "3", label: "Miercoles" },
+              { value: "4", label: "Jueves" },
+              { value: "5", label: "Viernes" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
+
+          <Input name="time" label="Hora" type="time" />
+        </form>
+      </Header>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
     </div>
-    )
+  );
 }
