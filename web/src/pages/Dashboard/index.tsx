b8bd0fc98622dc65e5da1ad73e17844c1178700b
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-08-12 02:08:46 -0300

    ortografyFix

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -21,7 +21,7 @@ import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
                 <div className="logo-container">
                     <img src={logoImg} alt="estudiosYa" />
                     <h2>
-                    Quedate en casa! Despreocupate y engresa en EstudiosYa</h2>
+                    Quedate en casa! Despreocupate y egresa en EstudiosYa</h2>
                   
                 </div>
             
