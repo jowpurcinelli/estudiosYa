3ee6cbfeffe3c32a2ca975c8a0a79b2a3811ce55
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-08-15 23:19:04 -0300

    cssToTs

---
+++ web/src/assets/styles/themes/dark.ts
@@ -0,0 +1,38 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    // primary: 'linear-gradient(90deg, #000000 0%, #312e38 100%)',
+    // secundary: 'linear-gradient(270deg, #FFE53B 0%, #FF2525 100%)',
+
+    // background: '#312E38',
+    li: '#232129',
+    text: '#FFFFFF',
+    inputColor: '#f4ede8',
+    inputPlaceholder: '#666360',
+    strongSchedule: '#999591',
+    appointmentBackground: '#3e3b47',
+    dayPicker: '#28262e',
+    dayAvailableBackground: '#3e3b47',
+    dayAvailableBackgroundHover: '#2f2c35',
+
+    background: '#000000',
+    primaryLighter: '#9871F5',
+    primaryLight: '#916BEA',
+    primary: '#0d0d0d',
+    primaryDark: '#774DD6',
+    primaryDarker: '#6842C2',
+    secondary: '#04D361',
+    secondaryDark: '#04BF58',
+    titleInPrimary: '#FFFFFF',
+    textInPrimary: '#E5E5E5',
+    textTitle: '#FFFFFF',
+    textComplement: '#E5E5E5',
+    textBase: '#E5E5E5',
+    lineInWhite: '#191919',
+    inputBackground: '#181818',
+    buttonText: '#FFFFFF',
+    boxBase: '#202020',
+    boxFooter: '#191919',
+  },
+}
---
+++ web/src/components/ThemeSwitcher/styles.tsx
@@ -0,0 +1,7 @@
+
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: absolute;
+  align-self: flex-start;
+  margin-top: 3vh;
