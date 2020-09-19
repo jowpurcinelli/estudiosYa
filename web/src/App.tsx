ffed41b7c5b94d143a474a923f734f8e21694f1e
jowpurcinelli <jppurcinelli@gmail.com>
2020-09-18 23:33:29 -0300

    fixingImports

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import './assets/styles/global.css';
 
 import  Routes from './routes'
-import Header from './components/Header';
-
-import Dashboard from './pages/Dashboard';
-
+i
 function App( ) {
     return (
         <div className="App">
